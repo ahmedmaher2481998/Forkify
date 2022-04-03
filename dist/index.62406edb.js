95de89a40d5e94661015ff51c19b1f4a392ae41c
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-04-03 22:43:03 +0200

    04 first api call sucess

---
+++ dist/index.62406edb.js
@@ -0,0 +1,26 @@
+const recipeContainer = document.querySelector('.recipe');
+const timeout = function(s) {
+    return new Promise(function(_, reject) {
+        setTimeout(function() {
+            reject(new Error(`Request took too long! Timeout after ${s} second`));
+        }, s * 1000);
+    });
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+let showRecipe = async function() {
+    try {
+        const res = await fetch("https://forkify-api.herokuapp.com/api/get?rId=47746");
+        let data = await res.json();
+        console.log(data);
+        // error handiling failed request
+        if (data.error) throw new Error(`${data.error}`);
+        let { recipe  } = data;
+        console.log(recipe);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
+
+//# sourceMappingURL=index.62406edb.js.map
--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,18 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+let showRecipe = async function () {
+  try {
+    const res = await fetch("https://forkify-api.herokuapp.com/api/get?rId=47746")
+    let data = await res.json();
+    console.log(data);
+    // error handiling failed request
+    if (data.error) throw new Error(`${data.error}`)
+    let { recipe } = data;
+
+    console.log(recipe);
+  } catch (err) {
+    alert(err)
+  }
+}
+showRecipe();
