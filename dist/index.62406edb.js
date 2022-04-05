37b15b7a948a171a56531ad5674d3744872e166e
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-04-05 20:03:41 +0200

    trying in vid 5

--- dist/index.62406edb.js
+++ dist/index.62406edb.js
@@ -10,13 +10,123 @@ const timeout = function(s) {
 ///////////////////////////////////////
 let showRecipe = async function() {
     try {
-        const res = await fetch("https://forkify-api.herokuapp.com/api/get?rId=47746");
+        const res = await fetch('https://forkify-api.herokuapp.com/api/get?rId=47746');
         let data = await res.json();
-        console.log(data);
         // error handiling failed request
         if (data.error) throw new Error(`${data.error}`);
         let { recipe  } = data;
-        console.log(recipe);
+        /* 
+    "recipe":{"publisher":"101 Cookbooks",
+    "ingredients":[
+      "4 1/2 cups (20.25 ounces) unbleached high-gluten,
+      bread, or all-purpose flour, chilled",
+      "1 3/4 (.44 ounce) teaspoons salt",
+      "1 teaspoon (.11 ounce) instant yeast",
+      "1/4 cup (2 ounces) olive oil (optional)",
+      "1 3/4 cups (14 ounces) water, ice cold (40F)",
+      "Semolina flour OR cornmeal for dusting"],
+     "source_url":"http://www.101cookbooks.com/archives/001199.html",
+     "recipe_id":"47746",
+     "image_url":"http://forkify-api.herokuapp.com/images/best_pizza_dough_recipe1b20.jpg",
+     "social_rank":100,
+     "publisher_url":"http://www.101cookbooks.com",
+    "title":"Best Pizza Dough Ever"}
+    */ // rendering the recipie
+        let markup = `  
+    <figure class="recipe__fig">
+          <img src="${recipe.image_url}" alt="${recipe.title}" class="recipe__img" />
+          <h1 class="recipe__title">
+            <span>${recipe.title}</span>
+          </h1>
+        </figure>
+
+        <div class="recipe__details">
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="src/img/icons.svg#icon-clock"></use>
+            </svg>
+            <span class="recipe__info-data recipe__info-data--minutes">${recipe.cook - time}</span>
+            <span class="recipe__info-text">minutes</span>
+          </div>
+          <div class="recipe__info">
+            <svg class="recipe__info-icon">
+              <use href="src/img/icons.svg#icon-users"></use>
+            </svg>
+            <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+            <span class="recipe__info-text">servings</span>
+
+            <div class="recipe__info-buttons">
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="src/img/icons.svg#icon-minus-circle"></use>
+                </svg>
+              </button>
+              <button class="btn--tiny btn--increase-servings">
+                <svg>
+                  <use href="src/img/icons.svg#icon-plus-circle"></use>
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div class="recipe__user-generated">
+            <svg>
+              <use href="src/img/icons.svg#icon-user"></use>
+            </svg>
+          </div>
+          <button class="btn--round">
+            <svg class="">
+              <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+            </svg>
+          </button>
+        </div>
+
+        <div class="recipe__ingredients">
+          <h2 class="heading--2">Recipe ingredients</h2>
+          <ul class="recipe__ingredient-list">
+            <li class="recipe__ingredient">
+              <svg class="recipe__icon">
+                <use href="src/img/icons.svg#icon-check"></use>
+              </svg>
+              <div class="recipe__quantity">1000</div>
+              <div class="recipe__description">
+                <span class="recipe__unit">g</span>
+                pasta
+              </div>
+            </li>
+
+            <li class="recipe__ingredient">
+              <svg class="recipe__icon">
+                <use href="src/img/icons.svg#icon-check"></use>
+              </svg>
+              <div class="recipe__quantity">0.5</div>
+              <div class="recipe__description">
+                <span class="recipe__unit">cup</span>
+                ricotta cheese
+              </div>
+            </li>
+          </ul>
+        </div>
+
+        <div class="recipe__directions">
+          <h2 class="heading--2">How to cook it</h2>
+          <p class="recipe__directions-text">
+            This recipe was carefully designed and tested by
+            <span class="recipe__publisher">The Pioneer Woman</span>. Please check out
+            directions at their website.
+          </p>
+          <a
+            class="btn--small recipe__btn"
+            href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/"
+            target="_blank"
+          >
+            <span>Directions</span>
+            <svg class="search__icon">
+              <use href="src/img/icons.svg#icon-arrow-right"></use>
+            </svg>
+          </a>
+        </div>
+      `;
     } catch (err) {
         alert(err);
     }
